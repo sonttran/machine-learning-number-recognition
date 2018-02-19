c91fa33b7c9cc9092a7da13e87551a104628ba98
Son Tran <son@qamera.com>
2018-02-19 12:21:47 -0800

    add README.md content

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # machine-learning-number-recognition
-Hand written number recognition by machine learning with MNIST dataset.
+Hand written number recognition by machine learning with MNIST dataset. Learning algorithms used are Histogram and Bayesian.
+
+### Dataset 
+* Dataset is obtained from <a href="http://yann.lecun.com/exdb/mnist/" target="_blank">Dr. Yan Lecun's website</a>Dr. Yan Lecun website. It's ready to be use and not modified in any way.
+* Each data record is a 28x28 pixel grid (784 dimensions vector). This pixel grid captures a hand written pattern of numbers (from 0 to 9). In this algorithm, we are only intested in number 1 and 9.
+* Selected data records of 1 and 9 in the dataset:
+![selected data](./img/selected data.png)
+
+
+### Data dimension reduction
+* Principle Component Analysis menthod is applied to reduce the dimension of data records.
+* Only two most significant principal components are selected to represent the data.
+* Dataset after dimension reduction:
+![Dimension reduced](./img/Dimension reduced.png)
+
+
+### Training and prediction
+* Histogram and Bayesian learning algorithms are built on dimension reduced dataset (2d)
+* A Random pair of 1 and 9 is chosen from dataset and applied to both algorithms for prediction. In this particular case, results are quite accrurate:
+![Prediction results](./img/Prediction results.png)
+
+
+### Training accruracy
+* Both algorithms are used on the whole dataset to calculate the training accruracy:
+![training accuracy](./img/training accuracy.png)
--- main.py
+++ main.py
@@ -38,8 +38,6 @@ def load_mnist(dataset="training", selecteddigits=range(10), path='./MNIST_data'
     X = np.zeros((N, rows*cols), dtype=np.uint8)
     T = np.zeros((N, 1), dtype=np.uint8)
     
-    #Fill X from digitsdata
-    #Fill T from labelsdata
     for i in range(N):
         X[i] = digitsdata[indices[i]*rows*cols:(indices[i]+1)*rows*cols]
         T[i] = labelsdata[indices[i]]
@@ -53,55 +51,6 @@ def vectortoimg(v,show=True):
     
     
 
-
-
-def readExcelSheet1(excelfile):
-    from pandas import read_excel
-    return (read_excel(excelfile)).values
-
-def readExcelRange(excelfile,sheetname="Sheet1",startrow=1,endrow=1,startcol=1,endcol=1):
-    from pandas import read_excel
-    values=(read_excel(excelfile, sheetname,header=None)).values;
-    return values[startrow-1:endrow,startcol-1:endcol]
-
-def readExcel(excelfile,**args):
-    if args:
-        data=readExcelRange(excelfile,**args)
-    else:
-        data=readExcelSheet1(excelfile)
-    if data.shape==(1,1):
-        return data[0,0]
-    elif (data.shape)[0]==1:
-        return data[0]
-    else:
-        return data
-    
-def writeExcelData(x,excelfile,sheetname,startrow,startcol):
-    from pandas import DataFrame, ExcelWriter
-    from openpyxl import load_workbook
-    df=DataFrame(x)
-    book = load_workbook(excelfile)
-    writer = ExcelWriter(excelfile, engine='openpyxl') 
-    writer.book = book
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-    df.to_excel(writer, sheet_name=sheetname,startrow=startrow-1, startcol=startcol-1, header=False, index=False)
-    writer.save()
-    writer.close()
-    
-def getSheetNames(excelfile):
-    from pandas import ExcelFile
-    return (ExcelFile(excelfile)).sheet_names
-
-
-
-
-
-
-
-excelfile='./Assignment_3_ Submission_Template.xlsx'
-sheets=getSheetNames(excelfile)
-
-        
 X, T = load_mnist(dataset="training",selecteddigits=[1,9])
 print("Checking multiple training vectors by plotting images.\nBe patient:")
 plt.close('all')
@@ -113,6 +62,8 @@ for row in range(nrows):
         plt.subplot(nrows, ncols, row*ncols+col + 1)
         vectortoimg(X[np.random.randint(len(T))],show=False)
 plt.show()
+
+
 
 mu=np.mean(X, axis=0) # get mean vector d feature
 Z = X - mu # get mean matrix N sample d feature
@@ -165,15 +116,10 @@ plotP1P2(XX,TT)
 
 cp = np.cov(XX[TT==9,].T)
 cn = np.cov(XX[TT==1,].T)
-
-
-
 p1min = np.amin(XX[:,0])
 p1max = np.amax(XX[:,0])
 p2min = np.amin(XX[:,1])
 p2max = np.amax(XX[:,1])
-
-
 
 
 
@@ -195,42 +141,25 @@ Hn, Hp = Build2DHistogramClassifier(25, XX, p1min,p1max,p2min,p2max,T)
 
 truthp = TT[100]
 truthn = TT[109]
-#print(TT[100]) # 9 positive
-#print(TT[109]) # 1 negative
 xp = X[100] # selected a feature vector # 100
 zp = xp - mu # substract mean
 pp=np.dot(zp,V.T)[0:2] # get 2 first Principal components
 rp=np.dot(np.dot(zp,V.T),V)
 xrecp=(np.dot(pp,V[0:2,:]))+mu #Reconstruction using 2 components
-#print(xp)
-#print(zp)
-#print(pp)
-#print(rp)
-#print(xrecp)
+
 
 xn = X[109] # selected a feature vector # 109
 zn = xn - mu # substract mean
 pn=np.dot(zn,V.T)[0:2] # get 2 first Principal components
 rn=np.dot(np.dot(zn,V.T),V)
 xrecn=(np.dot(pn,V[0:2,:]))+mu #Reconstruction using 2 components
-#print(xn)
-#print(zn)
-#print(pn)
-#print(rn)
-#print(xrecn)
 
 
-
-def getFemaleOrMaleGivenHeightAndHandspan(B,x,htmin,htmax,hsmin,hsmax,HF,HM):
+def getPosOrNeg(B,x,htmin,htmax,hsmin,hsmax,HF,HM):
     heightBinIndex=(np.round(((B-1)*(x[0]-htmin)/(htmax-htmin)))).astype('int32')
     handSpanBinIndex=(np.round(((B-1)*(x[1]-hsmin)/(hsmax-hsmin)))).astype('int32')
     negCount = HF[int(heightBinIndex),int(handSpanBinIndex)]
     posCount = HM[int(heightBinIndex),int(handSpanBinIndex)]
-#    print ('given height & handspan', x)
-#    print ('binIndex', heightBinIndex, handSpanBinIndex)
-#    print ('F count', negCount)
-#    print ('M count', posCount)
-    
     if(negCount > posCount):
         label = 1
         prob = negCount/(negCount + posCount)
@@ -240,11 +169,9 @@ def getFemaleOrMaleGivenHeightAndHandspan(B,x,htmin,htmax,hsmin,hsmax,HF,HM):
     return label, prob
 
 
-
-def BayesianProbFemale(x, sigmaF, muF, NF, sigmaM, muM, NM):
+def posOrNegBayesian(x, sigmaF, muF, NF, sigmaM, muM, NM):
     negCount = 1/(2*np.pi*math.sqrt(np.linalg.det(sigmaF)))*np.exp(-0.5*(x-muF).dot((np.linalg.inv(sigmaF)).dot((x-muF).T)))*NF
     posCount = 1/(2*np.pi*math.sqrt(np.linalg.det(sigmaM)))*np.exp(-0.5*(x-muM).dot((np.linalg.inv(sigmaM)).dot((x-muM).T)))*NM
-    
     if(negCount > posCount):
         label = 1
         prob = negCount/(negCount + posCount)
@@ -258,40 +185,27 @@ def BayesianProbFemale(x, sigmaF, muF, NF, sigmaM, muM, NM):
 
 
 
-resultlabelHp, resultprobHp = getFemaleOrMaleGivenHeightAndHandspan(25,pp,p1min,p1max,p2min,p2max,Hn ,Hp)
-#print(resultlabelHp, resultprobHp)
-resultlabelBp, resultprobBp = BayesianProbFemale(pp, cn, mun, Nn, cp, mup, Np)
-#print(resultlabelBp, resultprobBp)
+resultlabelHp, resultprobHp = getPosOrNeg(25,pp,p1min,p1max,p2min,p2max,Hn ,Hp)
+resultlabelBp, resultprobBp = posOrNegBayesian(pp, cn, mun, Nn, cp, mup, Np)
     
-resultlabelHn, resultprobHn = getFemaleOrMaleGivenHeightAndHandspan(25,pn,p1min,p1max,p2min,p2max,Hn ,Hp)
-#print(resultlabelHn, resultprobHn)
-resultlabelBn, resultprobBn = BayesianProbFemale(pn, cn, mun, Nn, cp, mup, Np)
-#print(resultlabelBn, resultprobBn)
+resultlabelHn, resultprobHn = getPosOrNeg(25,pn,p1min,p1max,p2min,p2max,Hn ,Hp)
+resultlabelBn, resultprobBn = posOrNegBayesian(pn, cn, mun, Nn, cp, mup, Np)
 
 accuracyH = (resultprobHp + resultprobHn) / 2
 accuracyB = (resultprobBp + resultprobBn) / 2
 
 
+# create confustion matrix and calculate prediction accuracy
 hResults = np.zeros(len(TT))
 for i,b in enumerate(XX):
-    label, prob = getFemaleOrMaleGivenHeightAndHandspan(25,b,p1min,p1max,p2min,p2max,Hn ,Hp)
+    label, prob = getPosOrNeg(25,b,p1min,p1max,p2min,p2max,Hn ,Hp)
     hResults[i] = label
-
 
 bResults = np.zeros(len(TT))
 for i,b in enumerate(XX):
-    label, prob = BayesianProbFemale(b, cn, mun, Nn, cp, mup, Np)
+    label, prob = posOrNegBayesian(b, cn, mun, Nn, cp, mup, Np)
     bResults[i] = label
-    
-print(bResults)
 
-#hResultsOne = hResults[TT==1]
-#TTone = TT[TT==1]
-#
-#hResultsNine = hResults[TT==9]
-#TTnine = TT[TT==9]
-#print((hResultsNine))
-#print((TTnine))
 
 def calAccuracy(cRes, TT):
     count = 0
@@ -305,85 +219,6 @@ print(accuracyH)
 print(accuracyB)
 
 
-#
-#
-#
-#for i,b in enumerate(hResults):
-#    if hResults[i] == TT[]:
-#        fname_digits = path + '/' + 'train-images-idx3-ubyte'
-#        fname_labels = path + '/' + 'train-labels-idx1-ubyte'
-#        elif dataset == "testing":
-#            fname_digits = path + '\\' + 't10k-images.idx3-ubyte'
-#            fname_labels = path + '\\' + 't10k-labels.idx1-ubyte'
-#            else:
-#                raise ValueError("dataset must be 'testing' or 'training'")
-    
-#print(TT)
-#print((hResults))
-#print(pp)
 
-
-def f(x): return x**2 
-print(f(8))
-
-g = lambda x: x**2 
-print(g(8))
 
     
-
-
-#writeExcelData([mu],excelfile,'Results',2,2)
-#writeExcelData([v1],excelfile,'Results',3,2)
-#writeExcelData([v2],excelfile,'Results',4,2)
-#print("Written mu, v1, v2")
-##
-#writeExcelData([Np,Nn],excelfile,'Results',6,2)
-#print("Written Np, Nn")
-#
-#writeExcelData([mup,mun],excelfile,'Results',9,2)
-#print("Written mup, mun")
-#
-#writeExcelData(cp,excelfile,'Results',12,2)
-#writeExcelData(cn,excelfile,'Results',14,2)
-#print("Written cp, cn")
-#
-#writeExcelData([[p1min,p1max]],excelfile,'Results',17,2)
-#writeExcelData([[p2min,p2max]],excelfile,'Results',18,2)
-#print("Written p1min, p1max, p2min, p2max")
-#
-#writeExcelData(Hp,excelfile,'Results',20,2)
-#writeExcelData(Hn,excelfile,'Results',46,2)
-#print("Written Hp, Hn")
-#
-#writeExcelData([xp],excelfile,'Results',74,2)
-#writeExcelData([zp],excelfile,'Results',75,2)
-#writeExcelData([pp],excelfile,'Results',76,2)
-#writeExcelData([rp],excelfile,'Results',77,2)
-#writeExcelData([xrecp],excelfile,'Results',78,2)
-#print("Written xp, zp, rp, pp, xrecp")
-#
-#writeExcelData([xn],excelfile,'Results',80,2)
-#writeExcelData([zn],excelfile,'Results',81,2)
-#writeExcelData([pn],excelfile,'Results',82,2)
-#writeExcelData([rn],excelfile,'Results',83,2)
-#writeExcelData([xrecn],excelfile,'Results',84,2)
-#print("Written xn, zn, rn, pn, xrecn")
-#
-#writeExcelData([truthp],excelfile,'Results',88,2)
-#writeExcelData([resultlabelHp],excelfile,'Results',89,2)
-#writeExcelData([resultprobHp],excelfile,'Results',89,3)
-#writeExcelData([resultlabelBp],excelfile,'Results',90,2)
-#writeExcelData([resultprobBp],excelfile,'Results',90,3)
-#print("Written truthp, resultlabelHp, resultprobHp, resultlabelBp, resultprobBp")
-#
-#writeExcelData([truthn],excelfile,'Results',92,2)
-#writeExcelData([resultlabelHn],excelfile,'Results',93,2)
-#writeExcelData([resultprobHn],excelfile,'Results',93,3)
-#writeExcelData([resultlabelBn],excelfile,'Results',94,2)
-#writeExcelData([resultprobBn],excelfile,'Results',94,3)
-#print("Written truthn, resultlabelHn, resultprobHn, resultlabelBn, resultprobBn")
-#
-#writeExcelData([accuracyH,accuracyB],excelfile,'Results',97,2)
-#print("Written accuracyH, accuracyB")
-#
-#print("Written everything!")
